27f45d4bbc76ac4dfcb8cb4c93788a61bd464782
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-07-01 14:28:01 +0300

    fugitive git split diff remaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,10 @@
+local function configuration ()
+	vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit)
+	vim.keymap.set("n", "<leader>gv", vim.cmd.Gvdiffsplit)
+	vim.keymap.set("n", "<leader>gh", vim.cmd.Ghdiffsplit)
+end
 return {
 	"tpope/vim-fugitive",
 	name = "Fugitive",
+	config = configuration,
 }
