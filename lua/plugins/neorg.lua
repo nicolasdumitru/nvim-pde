c96855ce5a5d62ecd8c2a4ca707f37f188e158d6
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-07-28 22:21:56 +0300

    added neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,30 @@
+local function configuration ()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {
+			  config = {
+				  folds = true,
+				  icon_preset = "basic",
+			  }
+		  }, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+end
+
+return {
+  {
+    "nvim-neorg/neorg",
+	name = "Neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = configuration,
+  },
+}
