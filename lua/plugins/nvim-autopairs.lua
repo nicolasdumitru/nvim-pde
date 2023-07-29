8adb662550c61518c119e98ac7afc5209d5dae8e
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-07-29 22:17:42 +0300

    autopairs - disabled example rules

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -31,19 +31,19 @@ local function configuration ()
 			check_comma = true,
 			manual_position = true,
 			highlight = 'Search',
-			highlight_grey='Comment'
+			highlight_grey = 'Comment'
 		},
 	})
 
 	local ts_conds = require('nvim-autopairs.ts-conds')
 
+	--[[
 	-- press % => %% only while inside a comment or string
 	npairs.add_rules({
 		Rule("%", "%", "lua")
 			:with_pair(ts_conds.is_ts_node({'string','comment'})),
-		Rule("$", "$", "lua")
-			:with_pair(ts_conds.is_not_ts_node({'function'}))
 	})
+	]]
 end
 
 return {
