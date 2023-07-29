2209bc7d98c8a9f696625c42f6d9a9c9431151e3
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-07-29 21:22:28 +0300

    indentation

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,30 +1,29 @@
 local function configuration ()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {
-			  config = {
-				  folds = true,
-				  icon_preset = "basic",
-			  }
-		  }, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-            },
-          },
-        },
-      }
+	require("neorg").setup {
+		load = {
+		["core.defaults"] = {}, -- Loads default behaviour
+		["core.concealer"] = {
+			config = {
+				folds = true,
+				icon_preset = "basic",
+			}
+		}, -- Adds pretty icons to your documents
+		["core.dirman"] = { -- Manages Neorg workspaces
+			config = {
+				workspaces = {
+					notes = "~/notes",
+				},
+				default_workspace = "notes",
+			},
+		},
+		},
+	}
 end
 
 return {
-  {
-    "nvim-neorg/neorg",
+	"nvim-neorg/neorg",
 	name = "Neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = configuration,
-  },
+	build = ":Neorg sync-parsers",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = configuration,
 }
