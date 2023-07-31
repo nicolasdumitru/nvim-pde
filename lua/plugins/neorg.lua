cc9ed7ebdb887405b8e466ee0e25987ba7df0544
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-07-31 20:36:28 +0300

    neorg - localleader ct toggle concealer

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -18,6 +18,7 @@ local function configuration()
 			},
 		},
 		vim.keymap.set("n", "<leader>nx", "<CMD>Neorg index<CR>"),
+		vim.keymap.set("n", "<localleader>ct", "<CMD>Neorg toggle-concealer<CR>"),
 	}
 
 	vim.opt.conceallevel = 2
