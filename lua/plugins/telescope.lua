8553dffd43d6a536849863f69826c7afe453e612
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-08-07 11:48:30 +0300

    telescope remaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 -- Telescope remaps
 local function remaps()
 	local builtin = require("telescope.builtin")
-	--vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-	vim.keymap.set("n", "<leader><Return>", builtin.find_files, {})
+	vim.keymap.set("n", "<leader><Return>", vim.cmd.Telescope, {})
+	vim.keymap.set("n", "<leader>f<Return>", builtin.find_files, {})
 	vim.keymap.set("n", "<leader>r<Return>", builtin.git_files, {})
 	vim.keymap.set("n", "<leader>g<Return>", builtin.live_grep, {})
 	vim.keymap.set("n", "<leader>b<Return>", builtin.buffers, {})
