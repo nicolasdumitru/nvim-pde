189a44d4e7c20338510ee6f82aabed78231977cc
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-09-18 18:02:32 +0300

    config structuring - file & buffer settings

--- lua/set.lua
+++ lua/set.lua
@@ -1,3 +1,19 @@
+-- Window title
+vim.opt.title = true
+
+-- Minimum amount of lines above and below cursor
+vim.opt.scrolloff = 4
+
+-- Split rightward and downward
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Tildeop (make tilde "~" behave like an operator)
+vim.opt.tildeop = true
+
+-- Open all folds
+vim.opt.foldenable = false
+
 -- Indentation settings
 local tab_width = 4
 vim.opt.shiftwidth = tab_width
@@ -12,31 +28,3 @@ vim.opt.expandtab = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
-
--- Update time
-vim.opt.updatetime = 50
-
--- Remember changes between sessions
-vim.opt.undofile = true
-
--- Use a swapfile for the buffer
-vim.opt.swapfile = true
-
--- Hidden (hide abbandoned buffers)
-vim.opt.hidden = true
-
--- Split rightward and downward
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Tildeop (make tilde "~" behave like an operator)
-vim.opt.tildeop = true
-
--- Open all folds
-vim.opt.foldenable = false
-
--- Minimum amount of lines above and below cursor
-vim.opt.scrolloff = 4
-
--- Window title
-vim.opt.title = true
