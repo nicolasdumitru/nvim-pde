3d837b5606386c5246cd7c4d0b648f1e56c279d7
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-10-11 22:06:28 +0300

    signcolumn - always show, width 1

--- lua/nick/core/ui.lua
+++ lua/nick/core/ui.lua
@@ -12,7 +12,7 @@ vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
 
 -- Signcolumn settings
-vim.opt.signcolumn = "auto:1-2"
+vim.opt.signcolumn = "yes:1"
 
 -- List mode (show invisible characters)
 vim.opt.list = true
