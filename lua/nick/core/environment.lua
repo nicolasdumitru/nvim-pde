478758a11daf7f3c72ea0fe0cce22606863fc07a
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-10-12 23:25:50 +0300

    environment - terminal & comments

--- lua/nick/core/environment.lua
+++ lua/nick/core/environment.lua
@@ -1,7 +1,13 @@
 -- Environment
 
+-- Programs
+TERM = os.getenv("TERM") or "alacritty"
+
+-- Directories
+-- shell-environment-variable directories
 HOME = os.getenv("HOME")
 XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or HOME .. "/.config"
 
+-- useful directories
 nvim_config_dir = XDG_CONFIG_HOME .. "/nvim"
 code = HOME .. "/code"
