cd1547ebc9647b77e3f952ec79ab6c27de0ad934
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-11-01 23:34:38 +0200

    ui - toggle relative numbers automatically

--- lua/nick/core/ui.lua
+++ lua/nick/core/ui.lua
@@ -7,6 +7,30 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Toggle relative numbers automatically
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+	pattern = "*",
+	group = augroup,
+	callback = function()
+		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+			vim.opt.relativenumber = true
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "CmdlineEnter", "WinLeave" }, {
+	pattern = "*",
+	group = augroup,
+	callback = function()
+		if vim.o.nu then
+			vim.opt.relativenumber = false
+			vim.cmd "redraw"
+		end
+	end,
+})
+
 -- Line/column highlighting
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
