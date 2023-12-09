5e1858aa81d411cd8a155797160c246da849e017
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-12-09 19:41:36 +0200

    telescope - keybindings, "theme" & formatting

--- lua/nick/plugin/spec/telescope.lua
+++ lua/nick/plugin/spec/telescope.lua
@@ -1,18 +1,38 @@
 local function configuration()
+	require("telescope").setup({
+		pickers = {
+			commands = {
+				theme = "ivy",
+			}
+		}
+	})
 	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<Leader>st", builtin.builtin, { desc = "Telescope builtin" })
-	vim.keymap.set("n", "<Leader>sf", builtin.find_files, { desc = "Search files" })
-	vim.keymap.set("n", "<Leader>sr", builtin.git_files, { desc = "Search files in git repo" })
+	vim.keymap.set("n", "<Leader>st", builtin.builtin,
+		{ desc = "Telescope builtin" })
+	vim.keymap.set("n", "<Leader>sf", builtin.find_files,
+		{ desc = "Search files" })
+	vim.keymap.set("n", "<Leader>sr", builtin.git_files,
+		{ desc = "Search files in git repo" })
 	vim.keymap.set("n", "<Leader>sg", builtin.live_grep, { desc = "Live grep" })
-	vim.keymap.set("n", "<Leader>s/",
-		function() builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown()) end,
+	vim.keymap.set("n", "<Leader>/",
+		function()
+			builtin.current_buffer_fuzzy_find(
+				require('telescope.themes').get_dropdown())
+		end,
 		{ desc = "Current buffer fuzzy find" })
-	vim.keymap.set("n", "<Leader>sb", builtin.buffers, { desc = "List open buffers" })
-	vim.keymap.set("n", "<Leader>so", builtin.oldfiles, { desc = "Search previously open files" })
+	vim.keymap.set("n", "<Leader>sb", builtin.buffers,
+		{ desc = "List open buffers" })
+	vim.keymap.set("n", "<Leader>so", builtin.oldfiles,
+		{ desc = "Search previously open files" })
 	vim.keymap.set("n", "<Leader>sh", builtin.help_tags, { desc = "List help" })
-	vim.keymap.set("n", "<Leader>sc", builtin.commands, { desc = "List plugin/user commands" })
-	vim.keymap.set("n", "<Leader>sk", builtin.keymaps, { desc = "List normal mode keymaps" })
-	vim.keymap.set("n", "<Leader>slr", builtin.lsp_references, { desc = "List LSP references" })
+	vim.keymap.set("n", "<Leader>:", builtin.commands,
+		{ desc = "List plugin/user commands" })
+	vim.keymap.set({ "n", "i" }, "<M-x>", builtin.commands,
+		{ desc = "List plugin/user commands" })
+	vim.keymap.set("n", "<Leader>sk", builtin.keymaps,
+		{ desc = "List normal mode keymaps" })
+	vim.keymap.set("n", "<Leader>slr", builtin.lsp_references,
+		{ desc = "List LSP references" })
 end
 
 return {
