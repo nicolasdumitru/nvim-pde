d39b4f59be6158c7025abd8a0670b4be80cc6203
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-12-28 00:11:19 +0200

    Neogit

---
+++ lua/nick/plugin/spec/neogit.lua
@@ -0,0 +1,10 @@
+return {
+	"NeogitOrg/neogit",
+	name = "Neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+	},
+	config = true,
+	event = "VeryLazy",
+}
