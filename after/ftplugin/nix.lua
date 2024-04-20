1bce77f82837c5f5ffdc518cd33cdeae70a7cece
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-04-20 20:07:33 +0300

    Nix ftplugin

---
+++ after/ftplugin/nix.lua
@@ -0,0 +1,8 @@
+-- Indentation
+local tab_width = 2
+vim.opt.shiftwidth = tab_width
+vim.opt.softtabstop = tab_width
+vim.opt.tabstop = tab_width
+vim.opt.varsofttabstop = nil
+vim.opt.vartabstop = nil
+vim.opt.expandtab = true
