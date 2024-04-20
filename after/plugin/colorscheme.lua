8b413b3af23ee412518b80262c01103309c2aeec
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-04-20 20:15:55 +0300

    moved gruvbox plugin theme configuration
    to gruvbox's plugin's file

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,38 +1,5 @@
 -- Colorscheme settings
 
--- Gruvbox theme options
--- setup must be called before loading the colorscheme
-require("gruvbox").setup({
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = true,
-		comments = true,
-		operators = false,
-		folds = true,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "", -- can be "hard", "soft" or empty string
-	palette_overrides = {
-		neutral_green = "#32db7a",
-		neutral_red = "#e7143c",
-	},
-	overrides = {
-		CursorLineNr = {
-			fg = "#fe8019",
-			bold = true,
-		},
-	},
-	dim_inactive = false,
-	transparent_mode = false,
-})
-
 -- Set the colorscheme
 vim.cmd.colorscheme("gruvbox")
 
