197ff835c0b1635c78a407b1cdf07b71563d0a01
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-04-25 21:45:18 +0300

    Tree-sitter - parsers
    Added the Tree-sitter query language & markdown parsers; organized the ordered
    the list.

--- lua/nick/plugin/spec/treesitter.lua
+++ lua/nick/plugin/spec/treesitter.lua
@@ -4,9 +4,13 @@ end
 
 local function configuration()
     require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "cpp", "rust", "haskell", "lua", "python",
-            "bash", "nix", "make", "latex", "bibtex", "vim", "vimdoc" },
+        -- A list of parser names
+        ensure_installed = {
+            "c", "lua", "vim", "vimdoc", "query", -- these should always be installed
+            "cpp", "rust", "haskell", "python", "bash",
+            "nix", "make",
+            "latex", "bibtex", "markdown", "markdown_inline"
+        },
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
 
