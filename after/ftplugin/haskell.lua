55055482f7324f0e8cbc82de4b2eb1be87c249be
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-06-11 17:46:36 +0300

    haskell: indentation settings

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,1 +1,9 @@
+-- Indentation
+local tab_width = 2
+vim.opt.shiftwidth = tab_width
+vim.opt.softtabstop = tab_width
+vim.opt.tabstop = tab_width
+vim.opt.varsofttabstop = nil
+vim.opt.vartabstop = nil
+vim.opt.smartindent = true
 vim.opt.expandtab = true
