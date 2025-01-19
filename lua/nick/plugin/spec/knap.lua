0004ea149a025c4eeb0035716fe32942a32c215d
nicolasdumitru <nicolasdumitru13@gmail.com>
2025-01-19 15:48:30 +0200

    KNAP: Turn keybinding comments into descriptions

--- lua/nick/plugin/spec/knap.lua
+++ lua/nick/plugin/spec/knap.lua
@@ -9,20 +9,19 @@ local function configuration()
         textopdfforwardjump = "okular --unique %outputfile%'#src:%line% '%srcfile%"
     }
 
-    -- set shorter name for keymap function
-    local kmap = vim.keymap.set
-    -- F5 processes the document once, and refreshes the view
-    kmap({ 'n', 'v', 'i' }, '<F5>', function() require("knap").process_once() end)
-    -- F6 closes the viewer application, and allows settings to be reset
-    kmap({ 'n', 'v', 'i' }, '<F6>', function() require("knap").close_viewer() end)
-    -- F7 toggles the auto-processing on and off
-    kmap({ 'n', 'v', 'i' }, '<F7>', function() require("knap").toggle_autopreviewing() end)
-    -- F8 invokes a SyncTeX forward search, or similar, where appropriate
-    kmap({ 'n', 'v', 'i' }, '<F8>', function() require("knap").forward_jump() end)
+    local kmap = vim.keymap.set -- set shorter name for keymap function
+    kmap({ 'n', 'v', 'i' }, '<F5>', function() require("knap").process_once() end,
+        { desc = "KNAP: process the document once, and refresh the view" })
+    kmap({ 'n', 'v', 'i' }, '<F6>', function() require("knap").close_viewer() end,
+        { desc = "KNAP: close the viewer application (allows settings to be reset)" })
+    kmap({ 'n', 'v', 'i' }, '<F7>', function() require("knap").toggle_autopreviewing() end,
+        { desc = "KNAP: toggle the auto-processing on and off" })
+    kmap({ 'n', 'v', 'i' }, '<F8>', function() require("knap").forward_jump() end,
+        { desc = "KNAP: invoke a SyncTeX forward search, or similar, where appropriate" })
 end
 
 return {
     "frabjous/knap",
-    ft = {"tex", "texmf", "texinfo", "initex", "plaintex", "bib", "bst"},
+    ft = { "tex", "texmf", "texinfo", "initex", "plaintex", "bib", "bst" },
     config = configuration()
 }
